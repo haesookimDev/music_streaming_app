f8def5b3e6096ef3f16d356822f6cc32e825f77e
soojin <soojin9911@naver.com>
2020-04-24 00:06:59 +0900

    음악/가수 검색버튼 구현

--- app/src/main/java/com/example/myapplication_scroll/MainActivity.java
+++ app/src/main/java/com/example/myapplication_scroll/MainActivity.java
@@ -2,14 +2,43 @@ package com.example.myapplication_scroll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_search;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.InitializeView();
+        this.SetListener();
     }
 
+    public void InitializeView() {
+        btn_search = (Button) findViewById(R.id.search_btn1);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void SetListener() {
+        View.OnClickListener Listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.search_btn1:
+                        textView.setTextColor(Color.RED);
+                        break;
+                }
+            }
+        };
+
+        btn_search.setOnClickListener(Listener);
+    }
 }
+
