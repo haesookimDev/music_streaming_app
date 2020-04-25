dcb5c608c81347868d9fc79da706f77b30409df3
soojin <soojin9911@naver.com>
2020-04-25 23:37:09 +0900

    화살표누르면 재현이 나온다. 임영웅누르면 임영웅 소개 페이지로 간다.

--- app/src/main/java/com/example/myapplication_scroll/MainActivity.java
+++ app/src/main/java/com/example/myapplication_scroll/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.myapplication_scroll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_search;
-    TextView textView;
-
+    ImageButton btn_favorite1;
+    ImageButton btn_favorite2;
+    ImageButton  btn_arrow;
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void InitializeView() {
         btn_search = (Button) findViewById(R.id.search_btn1);
-        textView = (TextView) findViewById(R.id.textView);
+        btn_favorite1 = (ImageButton) findViewById(R.id.favorite_btn1);
+        btn_favorite2 = (ImageButton) findViewById(R.id.favorite_btn2);
+        btn_arrow = (ImageButton) findViewById(R.id.arrow_btn);
     }
 
     public void SetListener() {
@@ -32,13 +38,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 switch (view.getId()) {
                     case R.id.search_btn1:
-                        textView.setTextColor(Color.RED);
+                        Intent intent_search = new Intent(getApplicationContext(),SearchActivity.class);
+                        startActivity(intent_search);
+                        break;
+                    case R.id.favorite_btn1:
+                        Intent intent_favorite = new Intent(getApplicationContext(),Favorite1Activity.class);
+                        startActivity(intent_favorite);
+                        break;
+                    case R.id.arrow_btn:
+                        i = 1-i;
+                        if(i==0){
+                            btn_favorite2.setVisibility(View.VISIBLE);
+                            btn_favorite1.setVisibility(View.INVISIBLE);
+                        }else{
+                            btn_favorite1.setVisibility(View.VISIBLE);
+                            btn_favorite2.setVisibility(View.INVISIBLE);
+                        }
                         break;
                 }
             }
         };
 
         btn_search.setOnClickListener(Listener);
+        btn_favorite1.setOnClickListener(Listener);
+        btn_arrow.setOnClickListener(Listener);
     }
 }
 
