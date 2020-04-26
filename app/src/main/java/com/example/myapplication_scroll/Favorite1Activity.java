17c75d0c0bf1bd8623f8ef02f1c0dcda6b98b687
soojin <soojin9911@naver.com>
2020-04-26 16:15:30 +0900

    임영웅 소개 창 만드는 중

--- app/src/main/java/com/example/myapplication_scroll/Favorite1Activity.java
+++ app/src/main/java/com/example/myapplication_scroll/Favorite1Activity.java
@@ -2,13 +2,22 @@ package com.example.myapplication_scroll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class Favorite1Activity extends AppCompatActivity {
+
+    RelativeLayout artist_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite1);
+
+
     }
+
 }
