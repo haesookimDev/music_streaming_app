2e7fb2646e3f2f5a106d10aabd6da182e4ab97d4
soojin <soojin9911@naver.com>
2020-04-27 23:19:03 +0900

    임영웅 페이지 작성 완료 nct127 페이지 작성중 xml 도형 파일 때문에 오류 발생

---
+++ app/src/main/java/com/example/myapplication_scroll/Favorite2Activity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication_scroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Favorite2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite2);
+    }
+}
