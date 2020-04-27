04492bbce69edf3a8ad27fcc44d481a6ba97d000
soojin <soojin9911@naver.com>
2020-04-28 00:54:04 +0900

    nct127페이지로 메인 페이지, 검색 페이지 이동 완료 임영웅 페이지로 메인 페이지, 검색 페이지 이동 완료

--- app/src/main/java/com/example/myapplication_scroll/Favorite1Activity.java
+++ app/src/main/java/com/example/myapplication_scroll/Favorite1Activity.java
@@ -2,22 +2,52 @@ package com.example.myapplication_scroll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class Favorite1Activity extends AppCompatActivity {
+    Button btn_mini_home;
+    Button btn_mini_search;
 
-    RelativeLayout artist_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite1);
 
+        this.InitializeView();
+        this.SetListener();
+    }
+    public void InitializeView() {
+        btn_mini_home = (Button) findViewById(R.id.mini_home_btn);
+        btn_mini_search = (Button) findViewById(R.id.mini_search_btn);
 
+    }
+
+    public void SetListener() {
+        View.OnClickListener Listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.mini_home_btn:
+                        Intent intent_main = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent_main);
+                        break;
+                    case R.id.mini_search_btn:
+                        Intent intent_search = new Intent(getApplicationContext(),SearchActivity.class);
+                        startActivity(intent_search);
+                        break;
+                }
+            }
+        };
+
+        btn_mini_home.setOnClickListener(Listener);
+        btn_mini_search.setOnClickListener(Listener);
     }
 
 }
--- app/src/main/java/com/example/myapplication_scroll/Favorite2Activity.java
+++ app/src/main/java/com/example/myapplication_scroll/Favorite2Activity.java
@@ -2,13 +2,49 @@ package com.example.myapplication_scroll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Favorite2Activity extends AppCompatActivity {
+    Button btn_mini_home;
+    Button btn_mini_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite2);
+
+        this.InitializeView();
+        this.SetListener();
+    }
+
+    public void InitializeView() {
+        btn_mini_home = (Button) findViewById(R.id.mini_home_btn);
+        btn_mini_search = (Button) findViewById(R.id.mini_search_btn);
+
+    }
+
+    public void SetListener() {
+        View.OnClickListener Listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.mini_home_btn:
+                        Intent intent_main = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent_main);
+                        break;
+                    case R.id.mini_search_btn:
+                        Intent intent_search = new Intent(getApplicationContext(),SearchActivity.class);
+                        startActivity(intent_search);
+                        break;
+                }
+            }
+        };
+
+        btn_mini_home.setOnClickListener(Listener);
+        btn_mini_search.setOnClickListener(Listener);
     }
 }
