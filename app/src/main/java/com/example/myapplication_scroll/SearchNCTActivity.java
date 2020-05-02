582a5ac12f37a3d37536683bded3e12218cc485c
haesoo kim <ww232330@gmail.com>
2020-05-02 20:52:52 +0900

    nct검색 페이지 완성

---
+++ app/src/main/java/com/example/myapplication_scroll/SearchNCTActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication_scroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+
+public class SearchNCTActivity extends AppCompatActivity {
+    Button btn_play1;
+    RelativeLayout songLay1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_nct);
+
+        this.InitializeView();
+        this.SetListener();
+    }
+
+    public void InitializeView() {
+        btn_play1 = findViewById(R.id.play_btn1);
+        songLay1 = findViewById(R.id.song_lay1);
+    }
+
+    public void SetListener() {
+        btn_play1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                songLay1.setBackgroundResource(R.drawable.music_select);
+            }
+        });
+    }
+
+}
