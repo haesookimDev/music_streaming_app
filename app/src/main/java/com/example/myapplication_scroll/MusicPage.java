6a7275873d31cb151e09991f8e1cdc04ccf35830
Jiyoung <yellow5316@naver.com>
2020-05-03 14:12:17 +0900

    임영웅 노래 창이랑 자바 추가

---
+++ app/src/main/java/com/example/myapplication_scroll/MusicPage.java
@@ -0,0 +1,50 @@
+package com.example.myapplication_scroll;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MusicPage extends AppCompatActivity {
+    Button btn_mini_home;
+    Button btn_mini_search;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite1);
+
+        this.InitializeView();
+        this.SetListener();
+    }
+    public void InitializeView() {
+        btn_mini_home = (Button) findViewById(R.id.mini_home_btn);
+        btn_mini_search = (Button) findViewById(R.id.mini_search_btn);
+
+    }
+
+    public void SetListener() {
+        View.OnClickListener Listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.mini_home_btn:
+                        Intent intent_main = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent_main);
+                        break;
+                    case R.id.mini_search_btn:
+                        Intent intent_search = new Intent(getApplicationContext(), SearchActivity.class);
+                        startActivity(intent_search);
+                        break;
+                }
+            }
+        };
+
+        btn_mini_home.setOnClickListener(Listener);
+        btn_mini_search.setOnClickListener(Listener);
+    }
+
+}
