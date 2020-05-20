77d06f31815580d38f75a766482d0a9845046dec
soojin <soojin9911@naver.com>
2020-05-20 21:18:02 +0900

    스트리밍 메인페이지/ 스트리밍가사페이지/ 스트리밍 재생목록페이지 구현완료 기능은 미완성

---
+++ app/src/main/java/com/example/myapplication_scroll/StreamingMain.java
@@ -0,0 +1,14 @@
+package com.example.myapplication_scroll;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StreamingMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_streaming_main);
+    }
+}
