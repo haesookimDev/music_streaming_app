2684a3ff460e794847dcbcdacde26deb22274823
haesooKim <63633387+haesookimDev@users.noreply.github.com>
2022-10-02 20:15:40 +0900

    Update TaskServer.java

--- app/src/main/java/com/example/myapplication_scroll/TaskServer.java
+++ app/src/main/java/com/example/myapplication_scroll/TaskServer.java
@@ -1,5 +1,5 @@
 package com.example.myapplication_scroll;
 
 public class TaskServer {
-    public static final String ip = "http://ec2-15-165-75-106.ap-northeast-2.compute.amazonaws.com:9000/";
+    public static final String ip = "백엔드API호출ip";
 }
